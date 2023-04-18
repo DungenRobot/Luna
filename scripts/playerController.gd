86328051f8bf16b22b0324bc1aa9bbffaf7f5ae6
DungenRobot <104450078+DungenRobot@users.noreply.github.com>
2023-04-17 20:06:25 -0400

    Safety

--- scripts/playerController.gd
+++ scripts/playerController.gd
@@ -5,7 +5,14 @@ var gravity = 2
 
 var grav_accel = Vector3()
 
+var look_input:= Vector2()
+
 const DEFAULT_SPEED = 5
+const LOOK_SPEED = Vector2(-0.6, -0.3)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		look_input = event.relative
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,17 +21,32 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var vec_to_point = (position - point_gravity).normalized()
+	var vec_to_point = point_gravity.direction_to(global_position)
+
 	up_direction = vec_to_point
-	rotation = vec_to_point
 	
-	pass
+	#print(up_direction)
+	
+	var look_point = vec_to_point.rotated(Vector3.BACK, -PI/2)
+	
+	look_at(look_point)
+	#rotation = vec_to_point 
+	
+	look_input *= LOOK_SPEED
+	
+	$Camera3D.rotation_degrees.x += look_input.y
+	$Camera3D.rotation_degrees.y += look_input.x
+	$Camera3D.rotation_degrees.x = clampf($Camera3D.rotation_degrees.x, 0, 180)
+
+	look_input = Vector2.ZERO
 
 func _physics_process(delta):
 	
-	var vec_to_point = (position - point_gravity).normalized()
+	var vec_to_point = (global_position - point_gravity).normalized()
+	
 	
 	if is_on_floor():
+		#print("grounded")
 		grav_accel = vec_to_point * -gravity
 	else:
 		grav_accel += vec_to_point * -gravity * delta
